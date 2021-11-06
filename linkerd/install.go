7c3773a0901cc5bf1c10e371c6759a4f46a9edef
aisuko <urakiny@gmail.com>
2021-11-06 10:32:19 +0800

    Fixed the issue of gosec G307
    Signed-off-by: aisuko <urakiny@gmail.com>

--- linkerd/install.go
+++ linkerd/install.go
@@ -271,6 +271,7 @@ func installBinary(location, platform string, res *http.Response) error {
 	if err != nil {
 		return err
 	}
+	/* #nosec G307 */
 	defer func() {
 		if err := out.Close(); err != nil {
 			fmt.Println(err)
