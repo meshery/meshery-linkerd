64309da2c7796f9cc7b53119544bf5b9fccf392b
lolverae <alberto.olvera.espinosa@gmail.com>
2023-01-29 21:59:14 -0600

    Replace GetLatestReleaseNames with meshkit func
    Signed-off-by: lolverae <alberto.olvera.espinosa@gmail.com>

--- build/config.go
+++ build/config.go
@@ -11,6 +11,7 @@ import (
 	"github.com/layer5io/meshery-adapter-library/adapter"
 	"github.com/layer5io/meshery-linkerd/internal/config"
 
+	"github.com/layer5io/meshkit/utils"
 	"github.com/layer5io/meshkit/utils/manifests"
 	smp "github.com/layer5io/service-mesh-performance/spec"
 )
@@ -55,9 +56,8 @@ func init() {
 	//Initialize Metadata including logo svgs
 	f, _ := os.Open("./build/meshmodel_metadata.json")
 	defer func() {
-		err := f.Close()
-		if err != nil {
-			fmt.Println(err.Error())
+		if err := f.Close(); err != nil {
+			fmt.Printf("Error closing file: %s\n", err)
 		}
 	}()
 	byt, _ := io.ReadAll(f)
@@ -66,15 +66,11 @@ func init() {
 	wd, _ := os.Getwd()
 	WorkloadPath = filepath.Join(wd, "templates", "oam", "workloads")
 	MeshModelPath = filepath.Join(wd, "templates", "meshmodel", "components")
-	vs, err := config.GetLatestReleaseNames(30)
-	if len(vs) == 0 {
-		fmt.Println("dynamic component generation failure: ", err.Error())
+	AllVersions, _ = utils.GetLatestReleaseTagsSorted("istio", "istio")
+	if len(AllVersions) == 0 {
 		return
 	}
-	for _, v := range vs {
-		AllVersions = append(AllVersions, string(v))
-	}
-	LatestVersion = AllVersions[0]
+	LatestVersion = AllVersions[len(AllVersions)-1]
 	DefaultGenerationMethod = adapter.Manifests
 	names, err := config.GetFileNames("linkerd", "linkerd2", "charts/linkerd-crds/templates/**")
 	if err != nil {
